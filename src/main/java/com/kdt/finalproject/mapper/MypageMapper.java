82aa5df01264ac2c32f1444a3c535ffd3183a8cd
autochain303 <hyungwook.jo@gmail.com>
2023-06-23 17:38:18 +0900

    car

--- src/main/java/com/kdt/finalproject/mapper/MypageMapper.java
+++ src/main/java/com/kdt/finalproject/mapper/MypageMapper.java
@@ -1,9 +1,19 @@
 package com.kdt.finalproject.mapper;
 
 import java.util.List;
+import java.util.Map;
 
-import com.kdt.finalproject.vo.MemVO;
+import com.kdt.finalproject.vo.CarVO;
+import com.kdt.finalproject.vo.CwriteVO;
 
 public interface MypageMapper {
- List<MemVO> init();
+    Map<String, Object> getCar();
+
+    int addCar(CarVO cvo);
+
+    int addCarWrite(CwriteVO cwvo);
+
+    int updateCar(CarVO cvo);
+
+    int updateCarWrite(CwriteVO cwvo);
 }
