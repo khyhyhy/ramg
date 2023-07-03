2ae21b066b8c53277820fe77f300ba9e2636d89a
hamsang13 <hsn9300@gmail.com>
2023-07-03 17:01:59 +0900

    taksong 차량목록검색 연결

--- src/main/java/com/kdt/finalproject/controller/TaksongController.java
+++ src/main/java/com/kdt/finalproject/controller/TaksongController.java
@@ -1,5 +1,11 @@
 package com.kdt.finalproject.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.annotations.Param;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kdt.finalproject.service.TaksongService;
+import com.kdt.finalproject.vo.CarVO;
+import com.kdt.finalproject.vo.CwriteVO;
 import com.kdt.finalproject.vo.MemVO;
 import com.kdt.finalproject.vo.ServiceVO;
 import com.kdt.finalproject.vo.SwriteVO;
@@ -37,6 +48,11 @@ public class TaksongController {
  @RequestMapping("/taksong/")
  public ModelAndView index() {
   ModelAndView mv = new ModelAndView();
+  MemVO mvo1 = (MemVO) session.getAttribute("mvo");
+  if (mvo1 != null) {
+   MemVO mvo = service.selectmem(mvo1.getM_idx());
+   mv.addObject("mvo", mvo);
+  }
   mv.setViewName("taksong/index");
 
   return mv;
@@ -49,6 +65,7 @@ public class TaksongController {
   mv.addObject("lat", lat);
   mv.addObject("lng", lng);
   mv.setViewName("taksong/taksong");
+
   return mv;
  }
 
@@ -68,7 +85,7 @@ public class TaksongController {
 
  @RequestMapping("/taksong/select/")
  public ModelAndView select(@Param("nowlat") String nowlat, @Param("nowlng") String nowlng,
-   @Param("nowstate") String nowstate, @Param("nowcity") String nowcity) {
+   @Param("nowstate") String nowstate, @Param("nowcity") String nowcity, @Param("m_idx") String m_idx) {
   ModelAndView mv = new ModelAndView();
   List<SwriteVO> swar = new ArrayList<SwriteVO>();
   // double lat = 37.48489405082669;
@@ -135,8 +152,9 @@ public class TaksongController {
     ++idx;
    }
   }
-  // MemVO dummy = service.selectmem("2");
-  // mv.addObject("evo", dummy);
+  MemVO mem = service.selectmem(m_idx);
+  mv.addObject("mvo", mem);
+
   mv.addObject("servicear", swar);
   mv.setViewName("taksong/serviceinfo");
   return mv;
@@ -149,6 +167,126 @@ public class TaksongController {
   System.out.println("c_idx = " + s_c_idx);
   System.out.println("chargepersent = " + chargepersent);
   System.out.println("s_payment = " + s_payment1);
+  return mv;
+ }
+
+ @RequestMapping("/taksong/carAddr")
+ public ModelAndView carAddr(String c_idx) throws Exception {
+  ModelAndView mv = new ModelAndView();
+  System.out.println("carAdd진입");
+  CarVO c_vo = service.carList(c_idx);
+  mv.addObject("cvo", c_vo);
+
+  String state = c_vo.getC_state();
+  String city = c_vo.getC_city();
+  String addr1 = c_vo.getC_addr1();
+
+  System.out.println(state);
+  String url = "https://dapi.kakao.com/v2/local/search/address.json?query=";
+  String restapikey = "KakaoAK 2cb999b6cc6710c90da5322517fd9e95";
+
+  StringBuffer sb = new StringBuffer();
+  sb.append(state);
+  sb.append(" ");
+  sb.append(city);
+  sb.append(" ");
+  sb.append(addr1);
+
+  String addr = sb.toString();
+  // System.out.println(addr);
+
+  URL obj;
+
+  int lo;
+
+  try {
+   String address = URLEncoder.encode(addr, "UTF-8");
+   // System.out.println(address);
+   obj = new URL(url + address);
+
+   HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+   con.setRequestMethod("GET");
+   con.setRequestProperty("Authorization", restapikey);
+   con.setRequestProperty("content-type", "application/json");
+   con.setDoOutput(true);
+   con.setUseCaches(false);
+   con.setDefaultUseCaches(false);
+
+   Charset charset = Charset.forName("UTF-8");
+   BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), charset));
+
+   String inputLine = null;
+   StringBuffer response = new StringBuffer();
+
+   while ((inputLine = in.readLine()) != null) {
+    response.append(inputLine);
+   }
+
+   // response 객체를 출력해보자
+   // System.out.println("RES :" + response.toString());
+
+   JSONParser jsonParser = new JSONParser();
+   JSONObject json_data = (JSONObject) jsonParser.parse(response.toString());
+   JSONArray documents = (JSONArray) json_data.get("documents");
+   JSONObject data = (JSONObject) documents.get(0);
+
+   String x = (String) data.get("x");
+   String y = (String) data.get("y");
+
+   // System.out.println("x:" + x + ", y:" + y);
+
+   // -------------------- 고객 위치 값 구하기 끝 ----------------------------
+
+   // 충전 기사들의 위치값 가져오기
+   ServiceVO[] sar = service.getService(state);
+   System.out.println("sar length" + sar.length);
+   List<SwriteVO> swar = new ArrayList<SwriteVO>();
+
+   for (ServiceVO value : sar) {
+    // String s_x = value.getS_mapx();
+    // String s_y = value.getS_mapy();
+
+    // System.out.println("이동식차량 x:" + s_x + ", 이동식차량 y:" + s_y);
+
+    double lng = Double.valueOf(value.getS_mapy());// 기사위치
+    double lat = Double.valueOf(value.getS_mapx());
+    double lat2 = Double.parseDouble(y); // 고객 차량 y
+    double lng2 = Double.parseDouble(x); // 고객 차량 x
+    double dLat = Math.toRadians(lat - lat2);
+    double dLon = Math.toRadians(lng - lng2);
+
+    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+      + Math.cos(Math.toRadians(lat)) * Math.cos(Math.toRadians(lat2)) *
+        Math.sin(dLon / 2)
+        * Math.sin(dLon / 2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    double d = 6371 * c * 1000; // Distance in m
+    System.out.println("현재 지점 " + addr1 + "과 좌표값의 거리는 " + d + "m입니다.");
+
+    lo = (int) Math.round(d);
+
+    // System.out.println("lo==" + lo);
+    // System.out.println(value.getS_radius());
+    // --------------------------------------------------------------------------------
+
+    if (lo <= Integer.valueOf(value.getS_radius())) {
+
+     String s_idx = value.getS_idx();
+     SwriteVO swvo = service.radiuSwriteVO(s_idx);
+     swar.add(swvo);
+     mv.addObject("servicear", swar);
+    }
+
+   }
+
+  } catch (Exception e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  }
+  // System.out.println(ar.);
+  System.out.println("carAdd탈출준비 ");
+  mv.setViewName("taksong/e_serviceinfo");
   return mv;
  }
 }
---
+++ src/main/webapp/WEB-INF/jsp/taksong/e_serviceinfo.jsp
@@ -0,0 +1,157 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Insert title here</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+    crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+
+  </head>
+
+  <body>
+   <!--////////// Header Start ////////////-->
+   <jsp:include page="../main/mainH.jsp"></jsp:include>
+   <!--////////// Header end ////////////-->
+
+   <!--////////// Main start //////////////-->
+   <div class="container">
+    <main>
+     <p>"${mvo.m_idx}"</p>
+     <!-- <c:if test="${sessionScope.evo == null}"></c:if> -->
+
+     <div style="display: flex; justify-content: center; margin-top: 50px;">
+      <button type="button" onclick="location.href='??'">충전하기</button>
+      <button type="button" onclick="location.href='??'" style="margin-left: 50px; margin-right: 50px;">현재상황</button>
+      <button type="button" onclick="location.href='map'">이용내역</button>
+     </div>
+
+
+
+
+     <div class="text-center">
+      <div class="row">
+       <div class="col">
+        상단영역
+       </div>
+      </div>
+      <form action="/taksong/serviceok" method="post">
+       <div class="row">
+        <div class="col">
+         <div class="list-group" onclick="carinfo(this)">
+          <input type="radio" class="btn-check" name="car" id='car' autocomplete="off" />
+          <label class="btn btn-outline-secondary list-group-item list-group-item-action"
+           for="car">${sessionScope.mvo.m_idx}/${cvo.c_name}/${cvo.c_type}</label>
+          <input type="hidden" name="c_idx" value=${cvo.c_idx} />
+          <input type="hidden" id="tank" name="c_val3" value=${cvo.c_val3} />
+         </div>
+        </div>
+        <div class="col">
+         <ul class="list-group">
+
+          <c:forEach items="${servicear}" var="vo" varStatus="status">
+           <fieldset onclick="serviceinfo(this)">
+            <input type="radio" class="btn-check" name="service" id='service${status.index}' autocomplete="off">
+            <label class="btn btn-outline-secondary list-group-item list-group-item-action"
+             for="service${status.index}">${vo.svo.s_city}${vo.svo.s_radius}/${vo.mvo.m_name}/${vo.cvo.c_name}</label>
+            <input type="hidden" name="s_idx" value=${vo.svo.s_idx} />
+           </fieldset>
+          </c:forEach>
+         </ul>
+        </div>
+        <div class="col">
+         <label for="chargebar">충전량</label>
+         <input type="range" class="form-range custom-range" step="10" min="50" max="100" id="chargebar"
+          name="chargepersent" value="50" list="tickmarks" onchange="muckpho(this)">
+         <datalist id="tickmarks"></datalist>
+         <label for="mukpho">목표치&nbsp;&nbsp;:&nbsp;&nbsp;</label>
+         <p id="battery"></p>
+         <p id="muckpho"></p>
+        </div>
+       </div>
+       <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col">
+         <p>결제정보</p>
+         <p>서비스 비용 : </p>
+         <p id="cprice">예상&nbsp;충전&nbsp;비용&nbsp;:&nbsp;원</p>
+         <p>총 비용 : 20.000</p>
+        </div>
+       </div>
+       <div class="row">
+        <div class="col">
+         <div>
+          <input type="radio" class="btn-check" name="s_payment1" id="payment1" autocomplete="off" onclick="sinyoung()">
+          <label class="btn btn-outline-primary" for="payment1">신용카드</label>
+
+          <input type="radio" class="btn-check" name="s_payment1" id="payment2" autocomplete="off">
+          <label class="btn btn-outline-primary" for="payment2">무통장거래</label>
+          <select style="display: none;" id="sinyong" name="s_payment2">
+           <option></option>
+          </select>
+         </div>
+        </div>
+       </div>
+       <div class="row"><br />
+       </div>
+       <div class="row">
+        <div class="col"><button type="submit" class="btn btn-primary btn-lg">서비스 신청하기</button></div>
+       </div>
+     </div>
+     <input type="hidden" id="s_c_idx" name="s_c_idx" />
+     <input type="hidden" id="s_s_idx" name="s_s_idx" />
+     </form>
+    </main>
+   </div>
+   <!--////////// Main end //////////////-->
+   <!--////////// Foter start //////////////-->
+   <jsp:include page="../main/mainF.jsp"></jsp:include>
+   <!--////////// Foter end //////////////-->
+   <script>
+
+    var btank;
+    var khw = 34.72;
+
+    function sinyoung() {
+     document.getElementById("sinyong").style()
+    }
+
+    function carinfo(f) {
+     console.log("c_idx==/" + f.querySelector('input[name="c_idx"]').value);
+     console.log("c_val3==" + f.querySelector('input[name="c_val3"]').value);
+     btank = f.querySelector('input[name="c_val3"]').value
+     document.getElementById("battery").innerText = btank + "kWh";
+     document.getElementById("s_c_idx").value = f.querySelector('input[name="c_idx"]').value;
+    }
+
+    function serviceinfo(f) {
+     console.log("s_idx==" + f.querySelector('input[name="s_idx"]').value);
+     document.getElementById("s_s_idx").value = f.querySelector('input[name="s_idx"]').value;
+    }
+
+    function muckpho(e) {
+
+     document.getElementById("muckpho").innerText = e.value + "%"
+     var chargeprice = (btank / 100 * e.value) * khw
+     console.log(chargeprice);
+     console.log(btank);
+     chargeprice = Math.floor(chargeprice).toString();
+     chargeprice = chargeprice.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+     document.getElementById("cprice").innerHTML = "예상&nbsp;충전&nbsp;비용&nbsp;:&nbsp" + chargeprice + "원";
+    }
+
+   </script>
+  </body>
+
+  </html>
